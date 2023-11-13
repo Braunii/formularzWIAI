df6e9c85942f4f9ff3b0d1098c8026d27567c659
Braunii <110412027+Braunii@users.noreply.github.com>
2023-11-13 20:57:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function rezerwacja(){
+    var parts = document.getElementById('data').value.split("-");
+    var year = parseInt(parts[0], 10);
+    var month = parseInt(parts[1], 10) - 1;
+    var day = parseInt(parts[2], 10);
     var formData = {
         imie: document.getElementById('imie').value,
         nazwisko: document.getElementById('nazwisko').value,
-        data: document.getElementById('data').value,
+        rok: year,
+        miesiac: month,
+        dzien: day,
         trzezwy: document.getElementById('trzezwy').value,
         siedzenie: document.getElementById('siedzenie').value,
     }
@@ -17,3 +23,22 @@ form.addEventListener('submit', function(event) {
     window.location.href = 'wynik.html';
 });
 
+function sprawdzDate(input) {
+    var formatDaty = /^\d{4}-\d{2}-\d{2}$/;
+
+    if(!input.value.match(formatDaty)) {
+        alert('Data powinna być w formacie RRRR-MM-DD!');
+        input.value = "";
+    }
+
+    var parts = input.value.split("-");
+    var year = parseInt(parts[0], 10);
+    var month = parseInt(parts[1], 10) - 1;
+    var day = parseInt(parts[2], 10);
+
+    var data = new Date(year, month, day);
+    if(data.getFullYear() !== year || data.getMonth() !== month || data.getDate() !== day) {
+        alert('Wprowadzona data jest nieprawidłowa!');
+        input.value = "";
+    }
+}
